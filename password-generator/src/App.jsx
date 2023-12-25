705213a399f888add820289d3175a437e26dfe9f
Kashif <ahmed.kashif1001@gmail.com>
2023-12-25 12:20:05 +0500

    char and numbers pass

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -4,17 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8);
-  const [numberAllowed, setNumberAllowed] = useState(false)
-  const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("")
+  const [length , setLength] = useState(8);
+  const [numberAllowed , setNumberAllowed] = useState(false);
+  const [charAllowed , setCharAllowed] = useState(false);
+  const [password , setPassword] = useState("");
 
-  const passwordGenerator = useCallback(() => {
+  const passwordGenerator = useCallback(()=> {
+  
     let pass = ""
-    let str = 
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    
-  }, [length, numberAllowed, charAllowed, setPassword])
+    let str= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+   if(numberAllowed) str += "0123456789" 
+   if(charAllowed) str += "!£$%^&*(){}~#?/+=_-`"
+  
+  
+},[length, numberAllowed, charAllowed, setPassword])
+
+
+
 
   return (
     <>
