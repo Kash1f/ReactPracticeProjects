a58fb9eb6e246436b7157b0b97c8e1ef6e31e49b
Kashif <ahmed.kashif1001@gmail.com>
2023-12-29 23:49:20 +0500

    adding buttons to cal

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -11,6 +11,40 @@ function App() {
 
     <div className="calc">
       <h1 id="input">0</h1>
+      <div>
+      <button>C</button>
+      <button>&lt;</button>
+      <button>%</button>
+      <button>/</button>
+      </div>
+      <div>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>*</button>
+      </div>
+      <div>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      </div>
+      <div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      </div>
+      <div>
+      <button>0</button>
+      <button>00</button>
+      <button>.</button>
+      <button>=</button>
+      </div>
+
+
+
+
     </div>
    </div>
     </>
