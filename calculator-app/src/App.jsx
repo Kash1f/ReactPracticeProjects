39af64c0e1d2807b91ac71febbb0baf4bdbc525e
Kashif <ahmed.kashif1001@gmail.com>
2024-01-06 23:40:56 +0500

    added routing to web solutions project

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -4,7 +4,20 @@ import './App.css'
 
 function App() {
 
-  const [input, setInput] = useState(0)
+  const [input, setInput] = useState('500');
+
+  const handleButtonClick = (value) => {
+    if(value === 'C'){
+      setInput("")
+    } else if(value === '<'){
+      setInput(input.slice(0,-1));
+
+    }
+    else {
+      setInput(value)
+    }
+
+  }
 
 
   return (
@@ -14,34 +27,34 @@ function App() {
     <div className="calc">
       <h1 id="input">{input}</h1>
       <div>
-      <button onClick={()=>handleButtonClick('C')}>C</button>
-      <button onClick={()=>handleButtonClick('%')}>&lt;</button>
-      <button onClick={()=>handleButtonClick('<')}>%</button>
-      <button onClick={()=>handleButtonClick('/')}>/</button>
+      <button onClick={()=>{handleButtonClick('C')}}>C</button>
+      <button onClick={()=>{handleButtonClick('<')}}>&lt;</button>
+      <button onClick={()=>{handleButtonClick('%')}}>%</button>
+      <button onClick={()=>{handleButtonClick('/')}}>/</button>
       </div>
       <div>
-      <button onClick={()=>handleButtonClick('%')}>7</button>
-      <button onClick={()=>handleButtonClick('%')}>8</button>
-      <button onClick={()=>handleButtonClick('%')}>9</button>
-      <button onClick={()=>handleButtonClick('%')}>*</button>
+      <button onClick={()=>{handleButtonClick('7')}}>7</button>
+      <button onClick={()=>{handleButtonClick('8')}}>8</button>
+      <button onClick={()=>{handleButtonClick('9')}}>9</button>
+      <button onClick={()=>{handleButtonClick('*')}}>*</button>
       </div>
       <div>
-      <button onClick={()=>handleButtonClick('%')}>4</button>
-      <button onClick={()=>handleButtonClick('%')}>5</button>
-      <button onClick={()=>handleButtonClick('%')}>6</button>
-      <button onClick={()=>handleButtonClick('%')}>-</button>
+      <button onClick={()=>{handleButtonClick('4')}}>4</button>
+      <button onClick={()=>{handleButtonClick('5')}}>5</button>
+      <button onClick={()=>{handleButtonClick('6')}}>6</button>
+      <button onClick={()=>{handleButtonClick('-')}}>-</button>
       </div>
       <div>
-      <button onClick={()=>handleButtonClick('%')}>1</button>
-      <button onClick={()=>handleButtonClick('%')}>2</button>
-      <button onClick={()=>handleButtonClick('%')}>3</button>
-      <button onClick={()=>handleButtonClick('%')}>+</button>
+      <button onClick={()=>{handleButtonClick('1')}}>1</button>
+      <button onClick={()=>{handleButtonClick('2')}}>2</button>
+      <button onClick={()=>{handleButtonClick('3')}}>3</button>
+      <button onClick={()=>{handleButtonClick('+')}}>+</button>
       </div>
       <div>
-      <button onClick={()=>handleButtonClick('%')}>0</button>
-      <button onClick={()=>handleButtonClick('%')}>00</button>
-      <button onClick={()=>handleButtonClick('%')}>.</button>
-      <button onClick={()=>handleButtonClick('%')}>=</button>
+      <button onClick={()=>{handleButtonClick('0')}}>0</button>
+      <button onClick={()=>{handleButtonClick('0')}}>00</button>
+      <button onClick={()=>{handleButtonClick('%')}}>.</button>
+      <button onClick={()=>{handleButtonClick('%')}}>=</button>
       </div>
     </div>
     </div>
---
+++ solutions-website/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Router>
+      <Routes>
+
+        <Route/>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
