99dc5dc6f752a502db251d81721566daecc71cff
Kashif <ahmed.kashif1001@gmail.com>
2024-11-19 22:48:13 +0500

    react router dom, lucide react, recharts

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline text-center items-center'>App</div>
+    <div className='text-3xl font-bold underline text-center'>App's</div>
   )
 }
 
